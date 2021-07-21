38e9860b87cdcd9de5d527ec99c733ca34e97453
suhasshettyy <scshetty07@gmail.com>
2021-07-21 20:24:22 +0530

    [Suhas] Add: UC2-lineComparision

--- Linecomparision/linecomparision.cs
+++ Linecomparision/linecomparision.cs
@@ -12,7 +12,12 @@ namespace Linecomparision
             double y1;
             double x2;
             double y2;
-            double distance;
+            double x3;
+            double y3;
+            double x4;
+            double y4;
+            double distance1;
+            double distance2;
 
             Console.WriteLine("Enter the first co-ordinate");
             x1 = Convert.ToDouble(Console.ReadLine());
@@ -20,10 +25,23 @@ namespace Linecomparision
             Console.WriteLine("Enter the second co-ordinate");
             x2 = Convert.ToDouble(Console.ReadLine());
             y2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the third co-ordinate");
+            x3 = Convert.ToDouble(Console.ReadLine());
+            y3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the fourth co-ordinate");
+            x4 = Convert.ToDouble(Console.ReadLine());
+            y4 = Convert.ToDouble(Console.ReadLine());
 
-            distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-            Console.WriteLine("The length of the line is:" + distance);
 
+            distance1 = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("The length of the line one is:" + distance1);
+            distance2 = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
+            Console.WriteLine("The length of the line two is :" + distance2);
+
+            if (distance1 == distance2)
+                Console.WriteLine("Both lines are equal");
+            else
+                Console.WriteLine("lines are unequal");
         }
     }
 }
