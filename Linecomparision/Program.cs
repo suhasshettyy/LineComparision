6833b807deddb51d5844ccab54d0e99d8e0a8566
suhasshettyy <scshetty07@gmail.com>
2021-07-22 23:09:15 +0530

    [Suhas] Add: UC2-lineEquality

--- Linecomparision/Program.cs
+++ Linecomparision/Program.cs
@@ -6,8 +6,20 @@ namespace LineComparison
     {
         static void Main(string[] args)
         {
-            Line line = new Line(2, 4, 6, 8);
-            line.Length();
+            Line linecomp = new Line(12, 23, 22, 30);
+            double line1 = linecomp.LengthOfLine();
+            Line linecomp2 = new Line(22, 23, 20, 40);
+            double line2 = linecomp2.LengthOfLine();
+            Console.WriteLine(" The length of First line is : " + line1);
+            Console.WriteLine(" The length of First line is : " + line2);
+            if (line1 == line2)
+            {
+                Console.WriteLine("Length of two Lines are equals");
+            }
+            else
+            {
+                Console.WriteLine("Length of two lines are not equals");
+            }
 
         }
     }
--- Linecomparision/linecomparision.cs
+++ Linecomparision/linecomparision.cs
@@ -6,21 +6,21 @@ namespace LineComparison
 {
     public class Line
     {
-        public int x1, x2, y1, y2;
+        int x1, x2, y1, y2;
         public Line(int x1, int x2, int y1, int y2)
         {
             this.x1 = x1;
             this.x2 = x2;
             this.y1 = y1;
             this.y2 = y2;
-
         }
-
-        public void Length()
+        /// <summary>
+        /// Calculate length of line and return it
+        /// </summary>
+        public double LengthOfLine()
         {
-            double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-            Console.WriteLine("The length of the line is:" + length);
+            double length = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            return length;
         }
-
     }
 }
