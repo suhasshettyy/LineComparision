034ef5794a69cdb5f9ee8e6e865c8210e2d9c60b
suhasshettyy <scshetty07@gmail.com>
2021-07-22 23:13:30 +0530

    [Suhas] Add: UC2-lineEquality

--- Linecomparision/Program.cs
+++ Linecomparision/Program.cs
@@ -6,9 +6,9 @@ namespace LineComparison
     {
         static void Main(string[] args)
         {
-            Line linecomp = new Line(12, 23, 22, 30);
+            Line linecomp = new Line(15, 26, 32, 40);
             double line1 = linecomp.LengthOfLine();
-            Line linecomp2 = new Line(22, 23, 20, 40);
+            Line linecomp2 = new Line(28, 24, 30, 20);
             double line2 = linecomp2.LengthOfLine();
             Console.WriteLine(" The length of First line is : " + line1);
             Console.WriteLine(" The length of First line is : " + line2);
